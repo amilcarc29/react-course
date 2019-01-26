c6007579481cd7844c385e49010621cf1ae36ea7
Amilcar Cattaneo <amilcar.cattaneo29@gmail.com>
2019-01-26 20:31:13 -0300

    Elimina ErrorBoundary de Persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,16 @@
 import React, {Component} from 'react';
 import Person from './Person/Person';
-// import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 
 class Persons extends Component {
 
     render() {
         return this.props.persons.map((person, index) => {
-            //return <ErrorBoundary key={person.id}>
             return  <Person
                         click={() => this.props.clicked(index)}
                         name={person.name}
                         age={person.age}
                         key={person.id}
                         changed={(event) => this.props.changed(event, person.id)} />
-              //  </ErrorBoundary>
         });
     }   
 }
